53f0bd6e324736d4f8c7b6b049b88056d9158c36
saku-56 <icecat3956@gmail.com>
2025-12-06 15:39:34 +0900

    change: ログイン後のトップページへの遷移先を変更 ログイン状態でトップページにアクセスしたら残薬一覧ページに遷移するように変更しました。

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:top]
   def top
+    if user_signed_in?
+      redirect_to leftover_medicines_path
+    end
   end
 end
