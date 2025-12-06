f6ad2762063f6bb3f4b1bd6427c7b10509cbf8a0
saku-56 <icecat3956@gmail.com>
2025-12-06 19:20:47 +0900

    feature:日本語化対応 カレンダーの表示が日本語になるようにファイルを追加しました。

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module Okan
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    config.i18n.default_locale = :ja
     config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
