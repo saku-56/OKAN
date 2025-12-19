22a3ff15c79e8796f57e2bdc6b9805dd7ce4307e
saku-56 <icecat3956@gmail.com>
2025-12-19 19:04:48 +0900

    feature: add_stockアクションの実装 ルーティング、ビュー、コントローラーを記述し、select_medicinesのビューファイルに遷移先を記述しました。

--- app/controllers/user_medicines_controller.rb
+++ app/controllers/user_medicines_controller.rb
@@ -31,15 +31,19 @@ class UserMedicinesController < ApplicationController
 
   def update_stock
      @user_medicine = current_user.user_medicines.find(params[:id])
-     
+
+     # 処方量を在庫量に加算
      additional_amount = user_medicine_params[:prescribed_amount].to_i
      @user_medicine.current_stock += additional_amount
 
+     # 処方日を更新
+     @user_medicine.date_of_prescription = user_medicine_params[:date_of_prescription]
+
      if @user_medicine.save
         redirect_to user_medicines_path, notice: "薬を追加しました"
-      else
+     else
         render :new, status: :unprocessable_entity
-      end
+     end
   end
 
   private
