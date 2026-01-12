742a5c4dfb8701edd55c791ced64723f9dca98ef
saku-56 <icecat3956@gmail.com>
2026-01-12 18:32:38 +0900

    feature: ログイン後のuser_medicineのシステムテストを記述

--- spec/system/user_medicines_spec.rb
+++ spec/system/user_medicines_spec.rb
@@ -13,4 +13,73 @@ RSpec.describe "UserMedicines", type: :system do
       end
     end
   end
+
+  describe 'ログイン後' do
+    before do
+      sign_in user
+    end
+
+    describe '薬の一覧表示' do
+      context '薬が登録されていない場合' do
+        it '「現在、薬はありません」というメッセージが表示される' do
+          visit user_medicines_path
+          expect(page).to have_content '現在、薬はありません'
+        end
+      end
+
+      context '薬が登録されている場合' do
+        let!(:user_medicine) { create(:user_medicine, user: user, medicine_name: 'aaa', current_stock: 5, dosage_per_time: 1) }
+
+        it '登録した薬が表示される' do
+          visit user_medicines_path
+          expect(page).to have_content 'aaa'
+          expect(page).to have_content '1回の服薬量：1錠'
+          expect(page).to have_content '選択'
+        end
+      end
+    end
+
+    describe '薬選択画面表示' do
+      context '登録が登録されている場合' do
+        let!(:user_medicine) { create(:user_medicine, user: user, medicine_name: 'aaa', current_stock: 5, dosage_per_time: 1) }
+
+        it '登録した薬が表示される' do
+          visit select_medicine_user_medicines_path
+          expect(page).to have_content 'aaa'
+          expect(page).to have_content '1回の服薬量：1錠'
+          expect(page).to have_content '選択'
+          expect(page).to have_content '新規登録'
+        end
+      end
+    end
+
+    describe '薬の新規登録' do
+      context 'フォームの入力値が正常' do
+        it '薬の新規作成が成功する' do
+          visit new_user_medicine_path
+          fill_in '薬名', with: 'aaa'
+          fill_in '1回の服薬量', with: '1'
+          fill_in '処方量', with: '30'
+          fill_in '処方日', with: Date.current
+          click_button '薬を追加'
+          expect(page).to have_content '薬を登録しました'
+          expect(current_path).to eq user_medicines_path
+        end
+      end
+    end
+
+    describe '薬の在庫追加' do
+      let!(:user_medicine) { create(:user_medicine, user: user, medicine_name: '風邪薬', current_stock: 5, dosage_per_time: 1) }
+      context 'フォームの入力値が正常' do
+        it '薬の在庫追加が成功する' do
+          visit add_stock_user_medicine_path(user_medicine)
+          fill_in '処方量', with: '30'
+          fill_in '処方日', with: Date.current
+          click_button '在庫を追加'
+          expect(page).to have_content '薬を追加しました'
+          expect(current_path).to eq user_medicines_path
+        end
+      end
+    end
+  end
 end
