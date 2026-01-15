07955a2cb5a17fcf799cdbca23ade7fbba636f18
saku-56 <icecat3956@gmail.com>
2026-01-15 15:41:22 +0900

    bugfix: 実際のパスワードの文字列を使用 FactoryBotで作成したユーザーのパスワードは暗号化されているため、user.passwordでは取得できないため書き換えました。

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -2,7 +2,7 @@ module LoginMacros
   def login(user)
     visit sign_in_path
     fill_in 'メールアドレス', with: user.email
-    fill_in 'パスワード', with: user.password
+    fill_in 'パスワード', with: 'password'
     click_on 'ログイン'
   end
 end
