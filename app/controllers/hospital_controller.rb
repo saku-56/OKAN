ea0afe8b33734a9f31fdefe59760f0c95e18e102
saku-56 <icecat3956@gmail.com>
2026-01-26 18:28:13 +0900

    feature: 病院情報新規作成 コントローラー、ルーティング、nested_attributesをモデルに書きました。

--- app/controllers/hospital_controller.rb
+++ app/controllers/hospital_controller.rb
@@ -2,4 +2,43 @@ class HospitalController < ApplicationController
   def index
     @hospital = current_user.hospitals
   end
+
+  def new
+    @hospital = current_user.hospitals.build
+
+   HospitalSchedule.day_of_weeks.keys.each do |day|
+      HospitalSchedule.periods.keys.each do |period|
+        @hospital.hospital_schedules.build(
+          day_of_week: day,
+          period: period
+        )
+      end
+    end
+  end
+
+  def create
+    @hospital = current_user.hospitals.build(hospital_params)
+
+    if @hospital.save
+      redirect_to @hospital, notice: "病院を登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def hospital_params
+    params.require(:hospital).permit(
+      :name,
+      :description,
+      hospital_schedules_attributes: [
+        :id,
+        :day_of_week,
+        :period,
+        :start_time,
+        :end_time
+      ]
+    )
+  end
 end
