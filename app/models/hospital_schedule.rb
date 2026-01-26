b08e84f697a146d3f97374dba3a096e748d49d40
saku-56 <icecat3956@gmail.com>
2026-01-26 20:21:25 +0900

    add: 休診を登録

--- app/models/hospital_schedule.rb
+++ app/models/hospital_schedule.rb
@@ -4,6 +4,12 @@ class HospitalSchedule < ApplicationRecord
   enum day_of_week: { monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6, holiday: 7 }
   enum period: { morning: 0, afternoon: 1 }
 
-  validates :day_of_week, presence: true
-  validates :period, presence: true
+  validates :day_of_week, presence: true, if: :has_time?
+  validates :period, presence: true, if: :has_time?
+
+  private
+
+  def has_time?
+    start_time.present? || end_time.present?
+  end
 end
