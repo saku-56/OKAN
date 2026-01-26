79d4a4aa53bb98eb83ef2f4fd6c61804f5083c16
saku-56 <icecat3956@gmail.com>
2026-01-26 18:28:35 +0900

    add: 病院情報新規作成のビューを記述

--- app/helpers/hospital_helper.rb
+++ app/helpers/hospital_helper.rb
@@ -1,2 +1,22 @@
 module HospitalHelper
+  def time_select_15min(form, field, start_hour, end_hour)
+    select_tag(
+      "#{form.object_name}[#{field}]",
+      options_for_select(
+        time_options(start_hour, end_hour),
+        form.object.send(field)&.strftime("%H:%M")
+      ),
+      include_blank: true,
+      class: "w-full px-1 py-1 text-xs border border-gray-300 rounded focus:ring-1 focus:ring-blue-500"
+    )
+  end
+
+  def time_options(start_hour, end_hour)
+    (start_hour...end_hour).flat_map do |hour|
+      [ 0, 15, 30, 45 ].map do |min|
+        time = format("%02d:%02d", hour, min)
+        [ time, time ]
+      end
+    end
+  end
 end
