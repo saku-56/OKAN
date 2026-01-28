7fb553ee55fdd62023ee44551f569c5714d23a26
saku-56 <icecat3956@gmail.com>
2026-01-28 14:24:20 +0900

    test: 病院情報の一覧表示と新規登録を作成しました。

---
+++ spec/system/hospitals_spec.rb
@@ -0,0 +1,119 @@
+require 'rails_helper'
+
+RSpec.describe 'Hospitals', type: :system do
+  let(:user) { create(:user) }
+
+  describe 'ログイン前' do
+    context '病院情報一覧ページへのアクセス' do
+      it 'ログインページにリダイレクトされる' do
+        visit hospital_index_path
+        expect(page).to have_content 'ログインしてください'
+        expect(current_path).to eq new_user_session_path
+      end
+    end
+  end
+
+  describe 'ログイン後' do
+    before do
+      sign_in user
+    end
+
+    describe '病院の一覧表示' do
+      context '病院が登録されていない場合' do
+        it '登録した病院が存在しない場合、メッセージが表示されること' do
+          visit hospital_index_path
+          expect(page).to have_content '現在、病院情報はありません'
+        end
+      end
+
+      context '病院情報が登録されている場合' do
+        let!(:hospital) { create(:hospital, user: user) }
+
+        it '登録した病院の一覧が表示されること' do
+          visit hospital_index_path
+
+          expect(page).to have_content(hospital.name)
+        end
+      end
+    end
+
+    describe '病院情報の新規作成' do
+      it '病院情報を登録できること' do
+        visit new_hospital_path
+
+        # 基本情報の入力
+        fill_in '病院名', with: 'クリニックA'
+        fill_in 'メモ', with: '予約優先です。'
+
+          # 月曜日の午前の時間を入力
+          select '09:00', from: 'hospital[hospital_schedules_attributes][0][start_time]'
+          select '12:00', from: 'hospital[hospital_schedules_attributes][0][end_time]'
+
+          # 月曜日の午後の時間を入力
+          select '14:00', from: 'hospital[hospital_schedules_attributes][1][start_time]'
+          select '18:00', from: 'hospital[hospital_schedules_attributes][1][end_time]'
+
+          # 火曜日の午前の時間を入力
+          select '09:00', from: 'hospital[hospital_schedules_attributes][2][start_time]'
+          select '12:00', from: 'hospital[hospital_schedules_attributes][2][end_time]'
+
+        click_button '登録'
+
+        # 登録成功の確認
+        expect(page).to have_content '病院を登録しました'
+        expect(page).to have_content 'クリニックA'
+
+        # データベースの確認
+        hospital = Hospital.last
+        expect(hospital.name).to eq 'クリニックA'
+        expect(hospital.description).to eq '予約優先です。'
+        expect(hospital.user).to eq user
+
+        # 16個のスケジュールが作成されていることを確認
+        hospital = Hospital.last
+        expect(hospital.hospital_schedules.count).to eq 16
+
+        # スケジュールの確認
+        monday_morning = hospital.hospital_schedules.find_by(day_of_week: :monday, period: :morning)
+        expect(monday_morning).to be_present
+        expect(monday_morning.start_time.strftime('%H:%M')).to eq '09:00'
+        expect(monday_morning.end_time.strftime('%H:%M')).to eq '12:00'
+
+        monday_afternoon = hospital.hospital_schedules.find_by(day_of_week: :monday, period: :afternoon)
+        expect(monday_afternoon).to be_present
+        expect(monday_afternoon.start_time.strftime('%H:%M')).to eq '14:00'
+        expect(monday_afternoon.end_time.strftime('%H:%M')).to eq '18:00'
+      end
+
+      it '病院名が空の場合はバリデーションエラーが表示されること' do
+        visit new_hospital_path
+
+          # 病院名を入力せずに診療時間だけ入力
+          select '09:00', from: 'hospital[hospital_schedules_attributes][0][start_time]'
+          select '12:00', from: 'hospital[hospital_schedules_attributes][0][end_time]'
+
+        click_button '登録'
+
+        # エラーメッセージの確認
+        expect(page).to have_content '病院名を入力してください'
+        expect(Hospital.count).to eq 0
+      end
+
+      it '開始時間が終了時間より後の場合はバリデーションエラーが表示されること' do
+        visit new_hospital_path
+
+        fill_in '病院名', with: 'テスト病院'
+
+          # 開始時間を終了時間より後に設定
+          select '12:00', from: 'hospital[hospital_schedules_attributes][0][start_time]'
+          select '09:00', from: 'hospital[hospital_schedules_attributes][0][end_time]'
+
+        click_button '登録'
+
+        # エラーメッセージの確認
+        expect(page).to have_content '終了時間は開始時間より遅い時刻に設定してください'
+        expect(Hospital.count).to eq 0
+      end
+    end
+  end
+end
