08bc0f0d1617d1a465b9375dd1bf4f23b6e53db0
saku-56 <icecat3956@gmail.com>
2026-01-28 11:51:49 +0900

    test: hospital_schedulesのmodelスペックを記述

--- spec/factories/hospital_schedules.rb
+++ spec/factories/hospital_schedules.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :hospital_schedule do
+    association :hospital
+    day_of_week { :monday }
+    period { :morning }
+    start_time { '09:00' }
+    end_time { '12:00' }
   end
 end
