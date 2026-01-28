6f3cd7338ab057238301d1921a8a12917500d37f
saku-56 <icecat3956@gmail.com>
2026-01-29 08:46:12 +0900

    test: 診療時間が表形式で表示されるテストを記述

--- spec/system/hospitals_spec.rb
+++ spec/system/hospitals_spec.rb
@@ -1,35 +1,35 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Hospitals', type: :system do
+RSpec.describe "Hospitals", type: :system do
   let(:user) { create(:user) }
 
-  describe 'ログイン前' do
-    context '病院情報一覧ページへのアクセス' do
-      it 'ログインページにリダイレクトされる' do
+  describe "ログイン前" do
+    context "病院情報一覧ページへのアクセス" do
+      it "ログインページにリダイレクトされる" do
         visit hospital_index_path
-        expect(page).to have_content 'ログインしてください'
+        expect(page).to have_content "ログインしてください"
         expect(current_path).to eq new_user_session_path
       end
     end
   end
 
-  describe 'ログイン後' do
+  describe "ログイン後" do
     before do
       sign_in user
     end
 
-    describe '病院の一覧表示' do
-      context '病院が登録されていない場合' do
-        it '登録した病院が存在しない場合、メッセージが表示されること' do
+    describe "病院の一覧表示" do
+      context "病院が登録されていない場合" do
+        it "登録した病院が存在しない場合、メッセージが表示されること" do
           visit hospital_index_path
-          expect(page).to have_content '現在、病院情報はありません'
+          expect(page).to have_content "現在、病院情報はありません"
         end
       end
 
-      context '病院情報が登録されている場合' do
+      context "病院情報が登録されている場合" do
         let!(:hospital) { create(:hospital, user: user) }
 
-        it '登録した病院の一覧が表示されること' do
+        it "登録した病院の一覧が表示されること" do
           visit hospital_index_path
 
           expect(page).to have_content(hospital.name)
@@ -37,36 +37,36 @@ RSpec.describe 'Hospitals', type: :system do
       end
     end
 
-    describe '病院情報の新規作成' do
-      it '病院情報を登録できること' do
+    describe "病院情報の新規作成" do
+      it "病院情報を登録できること" do
         visit new_hospital_path
 
         # 基本情報の入力
-        fill_in '病院名', with: 'クリニックA'
-        fill_in 'メモ', with: '予約優先です。'
+        fill_in "病院名", with: "クリニックA"
+        fill_in "メモ", with: "予約優先です。"
 
-          # 月曜日の午前の時間を入力
-          select '09:00', from: 'hospital[hospital_schedules_attributes][0][start_time]'
-          select '12:00', from: 'hospital[hospital_schedules_attributes][0][end_time]'
+        # 月曜日の午前の時間を入力
+        select "09:00", from: "hospital[hospital_schedules_attributes][0][start_time]"
+        select "12:00", from: "hospital[hospital_schedules_attributes][0][end_time]"
 
-          # 月曜日の午後の時間を入力
-          select '14:00', from: 'hospital[hospital_schedules_attributes][1][start_time]'
-          select '18:00', from: 'hospital[hospital_schedules_attributes][1][end_time]'
+        # 月曜日の午後の時間を入力
+        select "14:00", from: "hospital[hospital_schedules_attributes][1][start_time]"
+        select "18:00", from: "hospital[hospital_schedules_attributes][1][end_time]"
 
-          # 火曜日の午前の時間を入力
-          select '09:00', from: 'hospital[hospital_schedules_attributes][2][start_time]'
-          select '12:00', from: 'hospital[hospital_schedules_attributes][2][end_time]'
+        # 火曜日の午前の時間を入力
+        select "09:00", from: "hospital[hospital_schedules_attributes][2][start_time]"
+        select "12:00", from: "hospital[hospital_schedules_attributes][2][end_time]"
 
-        click_button '登録'
+        click_button "登録"
 
         # 登録成功の確認
-        expect(page).to have_content '病院を登録しました'
-        expect(page).to have_content 'クリニックA'
+        expect(page).to have_content "病院を登録しました"
+        expect(page).to have_content "クリニックA"
 
         # データベースの確認
         hospital = Hospital.last
-        expect(hospital.name).to eq 'クリニックA'
-        expect(hospital.description).to eq '予約優先です。'
+        expect(hospital.name).to eq "クリニックA"
+        expect(hospital.description).to eq "予約優先です。"
         expect(hospital.user).to eq user
 
         # 16個のスケジュールが作成されていることを確認
@@ -76,53 +76,53 @@ RSpec.describe 'Hospitals', type: :system do
         # スケジュールの確認
         monday_morning = hospital.hospital_schedules.find_by(day_of_week: :monday, period: :morning)
         expect(monday_morning).to be_present
-        expect(monday_morning.start_time.strftime('%H:%M')).to eq '09:00'
-        expect(monday_morning.end_time.strftime('%H:%M')).to eq '12:00'
+        expect(monday_morning.start_time.strftime("%H:%M")).to eq "09:00"
+        expect(monday_morning.end_time.strftime("%H:%M")).to eq "12:00"
 
         monday_afternoon = hospital.hospital_schedules.find_by(day_of_week: :monday, period: :afternoon)
         expect(monday_afternoon).to be_present
-        expect(monday_afternoon.start_time.strftime('%H:%M')).to eq '14:00'
-        expect(monday_afternoon.end_time.strftime('%H:%M')).to eq '18:00'
+        expect(monday_afternoon.start_time.strftime("%H:%M")).to eq "14:00"
+        expect(monday_afternoon.end_time.strftime("%H:%M")).to eq "18:00"
       end
 
-      it '病院名が空の場合はバリデーションエラーが表示されること' do
+      it "病院名が空の場合はバリデーションエラーが表示されること" do
         visit new_hospital_path
 
-          # 病院名を入力せずに診療時間だけ入力
-          select '09:00', from: 'hospital[hospital_schedules_attributes][0][start_time]'
-          select '12:00', from: 'hospital[hospital_schedules_attributes][0][end_time]'
+        # 病院名を入力せずに診療時間だけ入力
+        select "09:00", from: "hospital[hospital_schedules_attributes][0][start_time]"
+        select "12:00", from: "hospital[hospital_schedules_attributes][0][end_time]"
 
-        click_button '登録'
+        click_button "登録"
 
         # エラーメッセージの確認
-        expect(page).to have_content '病院名を入力してください'
+        expect(page).to have_content "病院名を入力してください"
         expect(Hospital.count).to eq 0
       end
 
-      it '開始時間が終了時間より後の場合はバリデーションエラーが表示されること' do
+      it "開始時間が終了時間より後の場合はバリデーションエラーが表示されること" do
         visit new_hospital_path
 
-        fill_in '病院名', with: 'テスト病院'
+        fill_in "病院名", with: "テスト病院"
 
-          # 開始時間を終了時間より後に設定
-          select '12:00', from: 'hospital[hospital_schedules_attributes][0][start_time]'
-          select '09:00', from: 'hospital[hospital_schedules_attributes][0][end_time]'
+        # 開始時間を終了時間より後に設定
+        select "12:00", from: "hospital[hospital_schedules_attributes][0][start_time]"
+        select "09:00", from: "hospital[hospital_schedules_attributes][0][end_time]"
 
-        click_button '登録'
+        click_button "登録"
 
         # エラーメッセージの確認
-        expect(page).to have_content '終了時間は開始時間より遅い時刻に設定してください'
+        expect(page).to have_content "終了時間は開始時間より遅い時刻に設定してください"
         expect(Hospital.count).to eq 0
       end
     end
 
-    describe '病院の詳細表示' do
+    describe "病院の詳細表示" do
       let!(:hospital) { create(:hospital, user: user) }
 
       # 複数の診療時間を作成
-      let!(:monday_morning) { create(:hospital_schedule, :monday, :morning, start_time: '09:00', end_time: '12:00', hospital: hospital) }
-      let!(:monday_afternoon) { create(:hospital_schedule, :monday, :afternoon, start_time: '14:00', end_time: '18:00', hospital: hospital) }
-      let!(:tuesday_morning) { create(:hospital_schedule, :tuesday, :morning, start_time: '10:00', end_time: '13:00', hospital: hospital) }
+      let!(:monday_morning) { create(:hospital_schedule, :monday, :morning, start_time: "09:00", end_time: "12:00", hospital: hospital) }
+      let!(:monday_afternoon) { create(:hospital_schedule, :monday, :afternoon, start_time: "14:00", end_time: "18:00", hospital: hospital) }
+      let!(:tuesday_morning) { create(:hospital_schedule, :tuesday, :morning, start_time: "10:00", end_time: "13:00", hospital: hospital) }
       # 休診の例(時間がnil)
       let!(:wednesday_morning) { create(:hospital_schedule, :wednesday, :morning, start_time: nil, end_time: nil, hospital: hospital) }
 
@@ -130,18 +130,42 @@ RSpec.describe 'Hospitals', type: :system do
         visit hospital_path(hospital)
       end
 
-      it '登録した全ての項目が表示されること' do
+      it "登録した全ての項目が表示されること" do
         expect(page).to have_content(hospital.name)
         expect(page).to have_content(hospital.description)
-        expect(page).to have_content('月')
-        expect(page).to have_content('9:00 - 12:00')
-        expect(page).to have_content('14:00 - 18:00')
-        expect(page).to have_content('火')
-        expect(page).to have_content('10:00 - 13:00')
+        expect(page).to have_content("月")
+        expect(page).to have_content("9:00 - 12:00")
+        expect(page).to have_content("14:00 - 18:00")
+        expect(page).to have_content("火")
+        expect(page).to have_content("10:00 - 13:00")
       end
 
-      it '休診日は適切に表示されること' do
-        expect(page).to have_content('ー')
+      it "休診日は適切に表示されること" do
+        expect(page).to have_content("ー")
+      end
+
+      it "診療時間が表形式で表示されること" do
+        # tableタグの存在確認
+        expect(page).to have_selector("table")
+
+        # ヘッダーの確認(曜日、午前、午後)
+        within "table thead" do
+          expect(page).to have_content("曜日")
+          expect(page).to have_content("午前")
+          expect(page).to have_content("午後")
+        end
+
+        # 曜日の表示
+        within "table tbody" do
+          expect(page).to have_content("月")
+          expect(page).to have_content("火")
+          expect(page).to have_content("水")
+          expect(page).to have_content("木")
+          expect(page).to have_content("金")
+          expect(page).to have_content("土")
+          expect(page).to have_content("日")
+          expect(page).to have_content("祝")
+        end
       end
     end
   end
