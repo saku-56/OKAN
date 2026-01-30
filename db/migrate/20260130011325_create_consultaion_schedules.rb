61aa75116964903ac76f8d2aff236ef443639c08
saku-56 <icecat3956@gmail.com>
2026-01-30 10:18:44 +0900

    add: CocsultationSchedulesのマイグレーションファイルをmigrate 必要なカラムと外部キー制約、null制約、デフォルト値を設定してmigtateしました。

---
+++ db/migrate/20260130011325_create_consultaion_schedules.rb
@@ -0,0 +1,11 @@
+class CreateConsultaionSchedules < ActiveRecord::Migration[7.2]
+  def change
+    create_table :consultaion_schedules do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :hospital, null: false, foreign_key: true
+      t.date :visit_date, null: false
+      t.integer :status, default: 0, null: false
+      t.timestamps
+    end
+  end
+end
