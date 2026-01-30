69153c98e21caecd6ffda44db0ceef10976c5627
saku-56 <icecat3956@gmail.com>
2026-01-30 18:53:29 +0900

    fix: タイポを修正

--- app/models/consultation_schedule.rb
+++ app/models/consultation_schedule.rb
@@ -5,5 +5,5 @@ class ConsultationSchedule < ApplicationRecord
   enum :status, { scheduled: 0, completed: 1, cancelled: 2 }
 
   validates :visit_date, presence: true
-  validates :status, presence: :ture
+  validates :status, presence: :true
 end
