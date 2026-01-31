f5a95c9bd90895caf257487960e36b04738d545e
saku-56 <icecat3956@gmail.com>
2026-01-31 09:47:04 +0900

    add: scopeのupcpmingを定義 statusがscheduledでvisit_dateが未来のHospitalSchedulesを絞り込むscopeを定義しました。

--- app/models/consultation_schedule.rb
+++ app/models/consultation_schedule.rb
@@ -6,4 +6,6 @@ class ConsultationSchedule < ApplicationRecord
 
   validates :visit_date, presence: true
   validates :status, presence: :true
+
+  scope :upcoming, -> { where(status: :scheduled).where("visit_date >= ?", Date.current).order(:visit_date) }
 end
