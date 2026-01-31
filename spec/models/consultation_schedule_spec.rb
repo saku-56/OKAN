f134cb5ddb32bb72375479d9aefec279dd70b7da
saku-56 <icecat3956@gmail.com>
2026-01-31 18:34:31 +0900

    test: consultaiton_scheduleのモデルスペックを作成

--- spec/models/consultation_schedule_spec.rb
+++ spec/models/consultation_schedule_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe ConsultationSchedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:hospital) { create(:hospital) }
+
+  describe "バリデーションチェック" do
+    it "設定したすべてのバリデーションが機能しているか" do
+      consultation_schedule = build(:consultation_schedule, user: user)
+      expect(consultation_schedule).to be_valid
+      expect(consultation_schedule.errors).to be_empty
+    end
+
+    describe "visit_date" do
+      it "visit_dateがない場合にバリデーションが機能してinvalidになるか" do
+        consultation_schedule = build(:consultation_schedule, user: user, visit_date: nil)
+        expect(consultation_schedule).to be_invalid
+        expect(consultation_schedule.errors[:visit_date]).to include("を入力してください")
+      end
+
+      it "visit_dateが過去の場合にカスタムバリデーションが機能してinvalidになるか" do
+        consultation_schedule = build(:consultation_schedule, user: user, visit_date: Date.yesterday)
+        expect(consultation_schedule).to be_invalid
+        expect(consultation_schedule.errors[:visit_date]).to be_present
+      end
+    end
+
+    describe "status" do
+      it "statusがない場合にバリデーションが機能してinvalidになるか" do
+        consultation_schedule = build(:consultation_schedule, user: user, status: nil)
+        expect(consultation_schedule).to be_invalid
+        expect(consultation_schedule.errors[:status]).to include("を入力してください")
+      end
+    end
+  end
+
+  describe "カラムのデフォルト値" do
+    let(:user) { create(:user) }
+    let(:hospital) { create(:hospital) }
+    it "statusのデフォルト値がscheduledであること" do
+      consultation_schedule = build(:consultation_schedule, user: user)
+       expect(consultation_schedule.status).to eq("scheduled")
+    end
+  end
+
+  describe "アソシエーション" do
+    it "userに属していること" do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it "hospitalに属していること" do
+      association = described_class.reflect_on_association(:hospital)
+      expect(association.macro).to eq :belongs_to
+    end
+  end
 end
