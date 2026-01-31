8f1240e5250ae9f7d56d874ddd1cb9aa7d5507a8
saku-56 <icecat3956@gmail.com>
2026-01-31 12:03:07 +0900

    feautre: 通院予定日を編集する機能を実装

--- app/controllers/consultation_schedules_controller.rb
+++ app/controllers/consultation_schedules_controller.rb
@@ -1,5 +1,6 @@
 class ConsultationSchedulesController < ApplicationController
-  before_action :set_hospital, only: %i[create]
+  before_action :set_hospital, only: %i[create update]
+  before_action :set_consultation_schedule, only: %i[update]
 
   def create
     @consultation_schedule = current_user.consultation_schedules.build(consultation_schedule_params)
@@ -15,10 +16,25 @@ class ConsultationSchedulesController < ApplicationController
     end
   end
 
+  def update
+    if @consultation_schedule.update(consultation_schedule_params)
+      redirect_to hospital_path(@hospital), notice: "通院予定日を変更しました。"
+    else
+      # @consultation_scheduleにエラー情報を含むオブジェクトごとビューの@next_visitに渡す
+      @next_visit = @consultation_schedule
+      flash.now[:danger] = "通院予定日変更に失敗しました。"
+      render "hospitals/show", status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_hospital
     @hospital = current_user.hospitals.find_by(uuid: params[:hospital_id])
+  end
+
+  def set_consultation_schedule
+    @consultation_schedule = current_user.consultation_schedules.find(params[:id])
   end
 
   def consultation_schedule_params
