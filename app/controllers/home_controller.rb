d21f7b68b7b9f6ffc7a7bd036750c7016fb67a45
saku-56 <icecat3956@gmail.com>
2026-02-04 09:44:05 +0900

    feature: 通院予定をカレンダーに表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,12 @@ class HomeController < ApplicationController
   def index
     @medicines_with_stock = current_user.user_medicines.has_stock.order(created_at: :asc)
     @date = params[:date]&.to_date
+    # 通院予定をハッシュで日付ごとに整理
+    @consultation_schedules_by_date = current_user.consultation_schedules
+                                                .includes(:hospital)
+                                                .where("visit_date >= ? AND visit_date <= ?",
+                                                       Date.current,
+                                                       Date.current + 6.months)
+                                                .group_by { |schedule| schedule.visit_date.to_date }
   end
 end
