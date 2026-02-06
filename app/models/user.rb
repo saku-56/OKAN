a77efac8f1f26b6f0c094786ab30cf22e95da0f9
saku-56 <icecat3956@gmail.com>
2026-02-06 16:58:08 +0900

    add: モデルファイルにline_user_idを取得する記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,10 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-      user.email = auth.info.email
+      user.email = auth.info.email || "#{auth.uid}@line.example.com"
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
+      user.line_user_id = auth.uid if auth.provider == "line"
     end
   end
 end
