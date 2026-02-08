ea16279d5474f5900c9028e794032326c2b8a110
saku-56 <icecat3956@gmail.com>
2026-02-08 16:30:28 +0900

    test: factory Botのuser_medicinesのtimes_per_dayを追加

--- spec/factories/user_medicines.rb
+++ spec/factories/user_medicines.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user_medicine do
     dosage_per_time { 1 }
+    times_per_day { 2 }
     prescribed_amount { 30 }
     date_of_prescription { Date.current }
     sequence(:uuid) { SecureRandom.uuid }
