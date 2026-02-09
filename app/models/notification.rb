6377c2af48c2ee2e5af929cf2d66d49d8cd321fe
saku-56 <icecat3956@gmail.com>
2026-02-09 09:50:53 +0900

    remove: バリデーションを設定しないので削除

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,6 +4,5 @@ class Notification < ApplicationRecord
   enum notification_type: { medicine_stock: "medicine_stock", consultation_reminder: "consultation_reminder" }
 
   validates :notification_type, presence: true, uniqueness: { scope: :user_id }
-  validates :enabled
   validates :days_before, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 7 }
 end
