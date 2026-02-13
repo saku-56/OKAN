25ca15131da486f52ea4bbb2f154db2d4e22a1a8
saku-56 <icecat3956@gmail.com>
2026-02-13 10:36:18 +0900

    test: LINEログインユーザーのFactoryBotを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,15 @@ FactoryBot.define do
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
+    # LINE連携済みユーザー
+    trait :with_line do
+      sequence(:line_user_id) { |n| "line_user_id_#{n}" }
+    end
+
+    # LINEでログインしたユーザー
+    trait :line_login do
+      sequence(:line_user_id) { |n| "line_uid_#{n}" }
+      email { "#{line_user_id}@line.example.com" }
+    end
   end
 end
