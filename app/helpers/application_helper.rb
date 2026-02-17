99ac82fe296266ee3b2f422c678eb766b7cba15b
saku-56 <icecat3956@gmail.com>
2026-02-17 19:39:59 +0900

    remove: フラッシュメッセージはnoticeとalertだけなので他を削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   # フラッシュメッセージの背景色
   def flash_background_color(type)
     case type.to_sym
-    when :notice, :success then "bg-green-200 text-green-800"
-    when :alert, :alert then "bg-red-300 text-red-800"
+    when :notice then "bg-green-200 text-green-800"
+    when :alert then "bg-red-300 text-red-800"
     else "bg-blue-200 text-blue-800"
     end
   end
