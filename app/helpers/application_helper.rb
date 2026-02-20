548ee2befd71ef1dd94031f4367772c21a9f6837
saku-56 <icecat3956@gmail.com>
2026-02-20 16:24:07 +0900

    refactor: 戻るボタンをヘルパーにメソッド化 pathが指定されたらそれを、されなかったらrootに飛ぶ、textが指定されたらそれを、されなかったら戻るを表示する。直前のURLがあれば遷移し、なければrootに飛ぶようにしました。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,19 @@ module ApplicationHelper
       "#{base_class} border-gray-300"
     end
   end
+
+  # 戻るボタン
+  def back_link(fallback_path = root_path, text: "戻る")
+    button_tag(
+      type: "button",
+      onclick: "if (document.referrer)
+                  { history.back(); }
+                else
+                  { window.location.href ='#{fallback_path}'; }",
+      class: "text-gray-800 hover:text-red-500 items-center gap-2 transition-colors duration-200 bg-transparent border-0 cursor-pointer",
+    ) do
+      content_tag(:i, "", class: "fa-solid fa-arrow-left fa-sm") +
+      content_tag(:span, text)
+    end
+  end
 end
