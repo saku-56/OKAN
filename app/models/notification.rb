824095bf96bd4cfc00edb1e0ae832ad74362b234
saku-56 <icecat3956@gmail.com>
2026-02-21 12:26:15 +0900

    change: enumの警告を修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :user
 
-  enum notification_type: { medicine_stock: "medicine_stock", consultation_reminder: "consultation_reminder" }
+  enum notification_type: { medicine_stock: "medicine_stock", consultation_reminder: "consultation_reminder" }, _prefix: true
 
   validates :notification_type, presence: true, uniqueness: { scope: :user_id }
   validates :days_before, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 7, allow_blank: true  }
