0903ec4a51511c4d6768ce6023580294c185865b
saku-56 <icecat3956@gmail.com>
2026-02-27 15:17:35 +0900

    add: メールの内容を作成

--- config/routes.rb
+++ config/routes.rb
@@ -39,4 +39,6 @@ Rails.application.routes.draw do
   get "notifications", to: "notifications#required"
 
   get "up" => "rails/health#show", as: :rails_health_check
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
